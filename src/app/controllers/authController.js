c6f4b03f745f039a8be8625489987393e0f53ebe
Lucas Soares Maciel <lucas.tibetano@gmail.com>
2023-01-16 07:41:52 -0300

    Adicionado a lógica de salvar a senha criptografada

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,13 +1,17 @@
 const router = require('express').Router()
-const {user} = require('../models')
+const { user } = require('../models')
+const bcrypt = require('bcryptjs')
 
 router.post('/register', async (req, res) => {
     try {
         const userTemp = { firstName, lastName, email, password } = req.body
+        const salt = await bcrypt.genSalt(10)
+        userTemp.password = await bcrypt.hash(userTemp.password, salt)
         const userRes = await user.create(userTemp)
+        userRes.password = undefined
         res.status(200).json(userRes)
     } catch (error) {
-        console.log(error)
+        //console.log(error)
         res.status(400).json("Erro no cadastro do usuario!")
     }
 })
